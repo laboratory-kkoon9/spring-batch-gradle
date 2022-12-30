35cfe4eb4eea623a65524b83ec69aad87714ba30
kkoon9 <rndrnjs2003@gmail.com>
2022-12-30 23:37:13 +0900

    feat: 쿼리 스트링 컨버터 개발
    Signed-off-by: kkoon9 <rndrnjs2003@gmail.com>

--- src/main/java/com/laboratorykkoon9/springbatchgradle/infra/boxoffice/util/QueryStringConverter.java
+++ src/main/java/com/laboratorykkoon9/springbatchgradle/infra/boxoffice/util/QueryStringConverter.java
@@ -1,2 +1,20 @@
-package com.laboratorykkoon9.springbatchgradle.infra.boxoffice.util;public class QueryStringConverter {
+package com.laboratorykkoon9.springbatchgradle.infra.boxoffice.util;
+
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.Map;
+
+public class QueryStringConverter {
+    private QueryStringConverter(){
+
+    }
+
+    public static String convert(Map<String, Object> messages) {
+        UriComponentsBuilder queryString = UriComponentsBuilder.newInstance();
+        for (String key : messages.keySet()) {
+            queryString.queryParam(key, messages.get(key).toString());
+        }
+
+        return queryString.toUriString();
+    }
 }
--- src/test/java/com/laboratorykkoon9/springbatchgradle/infra/boxoffice/util/QueryStringConverterTest.java
+++ src/test/java/com/laboratorykkoon9/springbatchgradle/infra/boxoffice/util/QueryStringConverterTest.java
@@ -1,2 +1,28 @@
-package com.laboratorykkoon9.springbatchgradle.infra.boxoffice.util;public class QueryStringConverterTest {
+package com.laboratorykkoon9.springbatchgradle.infra.boxoffice.util;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class QueryStringConverterTest {
+    @DisplayName("파라미터명과 값을 받아서 쿼리스트링 문자열로 컨버팅해준다.")
+    @Test
+    void test_1() {
+        // given
+        Map<String, Object> messages = new LinkedHashMap<>(); // Test를 위해 LinkedHashMap으로 선언하였습니다.
+        messages.put("key", "1234");
+        messages.put("targetDt", "20221230");
+
+        // when
+        String result = QueryStringConverter.convert(messages);
+        String expected = "?key=1234&targetDt=20221230";
+
+        // then
+        assertThat(result).isEqualTo(expected);
+
+    }
 }
