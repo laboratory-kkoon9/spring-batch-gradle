2148c2ff6eb09821cda61d129ac9c74da4a2dee0
kkoon9 <rndrnjs2003@gmail.com>
2023-01-01 22:37:17 +0900

    feat: 일일 박스오피스 검색 Dto
    Signed-off-by: kkoon9 <rndrnjs2003@gmail.com>

--- src/main/java/com/laboratorykkoon9/springbatchgradle/infra/boxoffice/dto/BoxOfficeRequestDto.java
+++ src/main/java/com/laboratorykkoon9/springbatchgradle/infra/boxoffice/dto/BoxOfficeRequestDto.java
@@ -1,2 +1,29 @@
-package com.laboratorykkoon9.springbatchgradle.infra.boxoffice.dto;public class BoxOfficeRequestDto {
+package com.laboratorykkoon9.springbatchgradle.infra.boxoffice.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+
+@Getter
+public class BoxOfficeRequestDto {
+    @JsonProperty("key")
+    private String key;
+    @JsonProperty("targetDt")
+    private String date;
+    @JsonProperty("itemPerPage")
+    private String row;
+    @JsonProperty("multiMovieYn")
+    private String isMulti;
+    @JsonProperty("repNationCd")
+    private String nation;
+    @JsonProperty("wideAreaCd")
+    private String area;
+
+    public BoxOfficeRequestDto(String key, String date, String row, String isMulti, String nation, String area) {
+        this.key = key;
+        this.date = date;
+        this.row = row;
+        this.isMulti = isMulti;
+        this.nation = nation;
+        this.area = area;
+    }
 }
