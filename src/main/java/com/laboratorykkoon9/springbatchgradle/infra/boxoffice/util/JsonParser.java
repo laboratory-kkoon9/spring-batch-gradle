d0d680a92749e15eb8f777528e29e2f3c39fe30d
kkoon9 <rndrnjs2003@gmail.com>
2023-01-04 22:11:38 +0900

    fix: mapper static으로 선언
    Signed-off-by: kkoon9 <rndrnjs2003@gmail.com>

--- src/main/java/com/laboratorykkoon9/springbatchgradle/infra/boxoffice/util/JsonParser.java
+++ src/main/java/com/laboratorykkoon9/springbatchgradle/infra/boxoffice/util/JsonParser.java
@@ -4,7 +4,8 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class JsonParser<T> {
+    private static final ObjectMapper mapper = new ObjectMapper();
     public String parse(T object) throws JsonProcessingException {
-        return new ObjectMapper().writeValueAsString(object);
+        return mapper.writeValueAsString(object);
     }
 }
